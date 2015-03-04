08223d6c522cff7b0542571d074d416463825d09
ugiacoman <ulises.giacoman@gmail.com>
2015-03-03 21:12:22 -0500

    change result

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 @app.route('/summary')
 def summary():
     song = request.args.get('a', 0, type=str)
-    return result=parse_it_asap(song)
+    return jsonify(result=parse_it_asap(song))
 
 if __name__ == "__main__":
     app.run(debug = True)
