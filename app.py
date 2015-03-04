96752af02b1b1ffc1c46fd729b3a8be2931a0ba8
ugiacoman <ulises.giacoman@gmail.com>
2015-03-03 22:41:39 -0500

    wait

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 @app.route('/summary')
 def summary():
     song = request.args.get('a', 0, type=str)
-    summed = str(parse_it_asap(song))
+    summed = 'hello world'
     return jsonify(result=summed)
 
 if __name__ == "__main__":
