0ce8750775c88b48ec41aecb9ad28f702e64d3eb
ugiacoman <ulises.giacoman@gmail.com>
2015-03-04 11:07:12 -0500

    added sys

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 import dogo
 import sys
 import logging
-
 app = Flask(__name__)
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -11,14 +10,13 @@ app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 
 @app.route('/_summary', methods=["GET"])
 def summary():
-    try:
-        song = request.args.get('a')    
-        return jsonify(result=dogo.parse_it_asap(song))
+	song = request.args.get('a')    
+	return jsonify(result=dogo.parse_it_asap(song))
 
 if __name__ == "__main__":
-    app.run(debug = True)
+	app.run(debug = True)
